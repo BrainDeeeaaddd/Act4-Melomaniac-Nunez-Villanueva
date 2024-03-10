274bd3f5016736449f953d96449a7b6b9e6c95d8
NunezJayson <nunez.jayson@mscmarinduque.edu.ph>
2024-03-10 18:47:27 +0800

    song 4

---
+++ song4.php
@@ -0,0 +1,130 @@
+<!-- song4.php -->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Song 4</title>
+</head>
+<body>
+    <h1>Song 4 - "Photograph" by Ed Sheeran </h1>
+    <?php
+    echo "<p>Lyrics for 'Photograph' by Ed Sheeran </p>";
+    echo "<pre>";
+    
+    echo "Lyrics and chords:<br>";
+    echo "<br>";
+    
+    echo "[Intro]<br>";
+    echo "D Bm A G<br>";
+    echo "<br>";
+    
+    echo "[Verse 1]<br>";
+    echo "D;<br>";
+    echo "Loving can hurt<br>";
+    echo "Bm;<br>";
+    echo "Loving can hurt sometimes<br>";
+    echo "A G;<br>";
+    echo "But it’s the only thing that I know<br>";
+    echo "D;<br>";
+    echo "When it gets hard<br>";
+    echo "Bm;<br>";
+    echo "You know it can get hard sometimes<br>";
+    echo "A G;<br>";
+    echo "It is the only thing that makes us feel alive<br>";
+    echo "<br>";
+    
+    echo "[Pre-Chorus]<br>";
+    echo "Bm G;<br>";
+    echo "We keep this love in a photograph<br>";
+    echo "D A;<br>";
+    echo "We make these memories for ourselves<br>";
+    echo "Bm;<br>";
+    echo "Where our eyes are never closing<br>";
+    echo "G;<br>";
+    echo "Our hearts were never broken<br>";
+    echo "D A;<br>";
+    echo "And times forever frozen still<br>";
+    echo "<br>";
+    
+    echo "[Chorus]<br>";
+    echo "D;<br>";
+    echo "So you can keep me inside the pocket of your<br>";
+    echo "A;<br>";
+    echo "Ripped jeans holding me closer till our<br>";
+    echo "Bm G;<br>";
+    echo "Eyes meet, you won’t ever be alone<br>";
+    echo "D;<br>";
+    echo "Wait for me to come home<br>";
+    echo "<br>";
+    
+    echo "[Verse 2]<br>";
+    echo "D;<br>";
+    echo "Loving can heal<br>";
+    echo "Bm;<br>";
+    echo "Loving can mend your soul<br>";
+    echo "A G;<br>";
+    echo "And it’s the only thing that I know<br>";
+    echo "D;<br>";
+    echo "I swear it will get easier<br>";
+    echo "Bm;<br>";
+    echo "remember that with every piece of ya<br>";
+    echo "A G;<br>";
+    echo "It is the only thing we take with us when we die<br>";
+    echo "<br>";
+    
+    echo "[Pre-Chorus]<br>";
+    echo "Bm G;<br>";
+    echo "We keep this love in a photograph<br>";
+    echo "D A;<br>";
+    echo "We make these memories for ourselves<br>";
+    echo "Bm;<br>";
+    echo "Where our eyes are never closing<br>";
+    echo "G;<br>";
+    echo "Our hearts were never broken<br>";
+    echo "D A;<br>";
+    echo "And times forever frozen still<br>";
+    echo "<br>";
+    
+    echo "[Chorus]<br>";
+    echo "D;<br>";
+    echo "So you can keep me inside the pocket of your<br>";
+    echo "A;<br>";
+    echo "Ripped jeans holding me closer till our<br>";
+    echo "Bm G;<br>";
+    echo "Eyes meet, you won’t ever be alone<br>";
+    echo "<br>";
+    
+    echo "[Bridge]<br>";
+    echo "D;<br>";
+    echo "And if you hurt me that’s ok baby, only<br>";
+    echo "A;<br>";
+    echo "Words bleed inside these pages you just<br>";
+    echo "Bm G;<br>";
+    echo "Hold me and I won’t ever let you go<br>";
+    echo "<br>";
+    
+    echo "[Interlude]<br>";
+    echo "Bm;<br>";
+    echo "Wait for me to come home<br>";
+    echo "G;<br>";
+    echo "Wait for me to come home<br>";
+    echo "D;<br>";
+    echo "Wait for me to come home<br>";
+    echo "A;<br>";
+    echo "Wait for me to come home<br>";
+    echo "<br>";
+    
+    echo "[Outro]<br>";
+    echo "D;<br>";
+    echo "Oh you can fit me inside the necklace you got when you were<br>";
+    echo "A;<br>";
+    echo "16 next to your heartbeat where I<br>";
+    echo "Bm G;<br>";
+    echo "Should be, keep it deep within your soul<br>";
+    echo "D;<br>";
+    echo "And if you hurt me that’s ok baby only<br>";
+    
+    ?>
+</body>
+</html>
