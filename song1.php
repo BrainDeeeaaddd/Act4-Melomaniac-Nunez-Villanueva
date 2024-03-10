ae63980a89ecbec891697a03706d772a7eb8bdbc
NunezJayson <nunez.jayson@mscmarinduque.edu.ph>
2024-03-10 18:45:45 +0800

    song 1

---
+++ song1.php
@@ -0,0 +1,69 @@
+<!-- song1.php -->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Song 1</title>
+</head>
+<body>
+    <h1>Song 1 - "Hey Jude" by Beatles </h1>
+    <?php
+
+    echo "[Interlude]<br>";
+    echo "F6 F7 C Csus4 C C F C7<br>";
+    echo "Da da da da da Da da da da<br><br>";
+
+    echo "[Verse 3]<br>";
+    echo "F C<br>";
+    echo "Hey Jude dont let me down<br>";
+    echo "C C* C6 C7 C7sus4 C7 F<br>";
+    echo "You have found her now go and get her<br>";
+    echo "Bb F<br>";
+    echo "Remember to let her into you heart<br>";
+    echo "C F Fmaj7 F7<br>";
+    echo "then you can start to make it better<br><br>";
+
+    echo "[Chorus]<br>";
+    echo "Bb Bb/A Gm<br>";
+    echo "So let it out and let it in hey Jude begin<br>";
+    echo "Gm7 C/E C F Fmaj7 F7<br>";
+    echo "your waiting for someone to perform with<br>";
+    echo "Bb Bb/A Gm<br>";
+    echo "And dont you know that its just you hey jude you’ll do<br>";
+    echo "Gm7 C/E C F<br>";
+    echo "The movement you need is on your shoulders<br><br>";
+
+    echo "[Interlude]<br>";
+    echo "F6 F7 C Csus4 C C F C7<br>";
+    echo "Da da da da da da da da da<br><br>";
+
+    echo "[Verse 4]<br>";
+    echo "F C<br>";
+    echo "Hey Jude dont make it bad<br>";
+    echo "F C7 C7sus4 C7 F<br>";
+    echo "take a sad song and make it better<br>";
+    echo "Bb F<br>";
+    echo "Remember to let her under your skin<br>";
+    echo "C F<br>";
+    echo "and then you’ll begin to make it better<br><br>";
+
+    echo "[Outro]<br>";
+    echo "F6 F7 Eb<br>";
+    echo "Na na na na na na na<br>";
+    echo "Bb<br>";
+    echo "Na na na na<br>";
+    echo "F<br>";
+    echo "Hey Jude<br>";
+    echo "F6 F7 Eb<br>";
+    echo "Na na na na na na na<br>";
+    echo "Bb<br>";
+    echo "Na na na na<br>";
+    echo "F<br>";
+    echo "Hey Jude<br>";
+
+    
+    ?>
+
+</body>
+</html>
