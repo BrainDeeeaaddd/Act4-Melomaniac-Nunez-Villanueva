5925a4241d2909cfc786429ddbca80800a89c3d6
NunezJayson <nunez.jayson@mscmarinduque.edu.ph>
2024-03-10 18:46:26 +0800

    song 2

---
+++ song2.php
@@ -0,0 +1,91 @@
+<!-- song2.php -->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Song 2</title>
+</head>
+<body>
+    <h1>Song 2 - "Hotel California" by Eagles </h1>
+    <?php
+    echo "<p>Lyrics for 'Hotel California' by Eagles </p>";
+    echo "<pre>";
+    
+    echo "Lyrics and chords:<br>";
+    echo "<br>";
+    
+    echo "[Verse 1]<br>";
+    echo "Bm F#<br>";
+    echo "On a dark desert highway, cool wind in my hair;<br>";
+    echo "A E<br>";
+    echo "Warm smell of colitas rising up through the air;<br>";
+    echo "G D<br>";
+    echo "Up ahead in the distance, I saw a shimmering light;<br>";
+    echo "Em<br>";
+    echo "My head grew heavy and my sight grew dim;<br>";
+    echo "F#<br>";
+    echo "I had to stop for the night;<br>";
+    echo "<br>";
+    
+    echo "[Verse 2]<br>";
+    echo "Bm F#<br>";
+    echo "Her mind is Tiffany-twisted, she got the Mercedes bends;<br>";
+    echo "A E<br>";
+    echo "She got a lot of pretty, pretty boys she calls friends;<br>";
+    echo "G D<br>";
+    echo "How they dance in the courtyard, sweet summer sweat.<br>";
+    echo "Em F#<br>";
+    echo "Some dance to remember, some dance to forget;<br>";
+    echo "<br>";
+    
+    echo "[Chorus]<br>";
+    echo "G D<br>";
+    echo "Welcome to the Hotel California.<br>";
+    echo "F#7 Bm<br>";
+    echo "Such a lovely place, such a lovely face;<br>";
+    echo "G D<br>";
+    echo "Plenty of room at the Hotel California;<br>";
+    echo "Em F#<br>";
+    echo "Any time of year (any time of year) you can find it here;<br>";
+    echo "<br>";
+    
+    echo "[Verse 3]<br>";
+    echo "Bm F#<br>";
+    echo "Mirrors on the ceiling, The pink champagne on ice;<br>";
+    echo "A E<br>";
+    echo "And she said ‘We are all just prisoners here, of our own device’;<br>";
+    echo "G D<br>";
+    echo "And in the master’s chambers, They gathered for the feast;<br>";
+    echo "Em<br>";
+    echo "They stab it with their steely knives,<br>";
+    echo "F#<br>";
+    echo "But they just can’t kill the beast;<br>";
+    echo "<br>";
+    
+    echo "[Chorus]<br>";
+    echo "G D<br>";
+    echo "Welcome to the Hotel California;<br>";
+    echo "F#7 Bm<br>";
+    echo "Such a lovely place (Such a lovely place), Such a lovely face;<br>";
+    echo "G D<br>";
+    echo "They livin’ it up at the Hotel California;<br>";
+    echo "Em F#<br>";
+    echo "What a nice surprise (what a nice surprise), Bring your alibis;<br>";
+    echo "<br>";
+    
+    echo "[Outro]<br>";
+    echo "Bm F#<br>";
+    echo "Last thing I remember, I was running for the door;<br>";
+    echo "A E<br>";
+    echo "I had to find the passage back to the place I was before;<br>";
+    echo "G D<br>";
+    echo "‘Relax,’ said the night man, ‘We are programmed to receive.<br>";
+    echo "Em<br>";
+    echo "You can check-out any time you like,<br>";
+    echo "F#<br>";
+    echo "But you can never leave!<br>";
+    
+    ?>
+</body>
+</html>
