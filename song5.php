25cbdfbe1e723ac9cf09e50a577e6d5082eb87cd
NunezJayson <nunez.jayson@mscmarinduque.edu.ph>
2024-03-10 18:47:58 +0800

    song 5

---
+++ song5.php
@@ -0,0 +1,138 @@
+<!-- song4.php -->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Song 4</title>
+</head>
+<body>
+    <h1>Song 4 - "Thingking Out Loud" by Ed Sheeran </h1>
+    <?php
+    echo "<p>Lyrics for 'Thinking Out Loud' by Ed Sheeran </p>";
+    echo "<pre>";
+    
+    echo "Lyrics and chords:<br>";
+    echo "<br>";
+    
+    echo "[Verse]<br>";
+    echo "C C/E F G;<br>";
+    echo "When your legs don’t work like they used to before;<br>";
+    echo "C C/E F G;<br>";
+    echo "And I can’t sweep you off of your feet;<br>";
+    echo "C C/E F G;<br>";
+    echo "Will your mouth still remember the taste of my love;<br>";
+    echo "C C/E F G;<br>";
+    echo "Will your eyes still smile from your cheeks;<br>";
+    echo "C C/E F G C C/E F G;<br>";
+    echo "And darling I will be loving you till we’re 70;<br>";
+    echo "C C/E F G C C/E;<br>";
+    echo "And baby my heart could still fall as hard at 23;<br>";
+    echo "F G;<br>";
+    echo "And I’m thinking bout how;<br>";
+    echo "<br>";
+    
+    echo "[Pre-Chorus]<br>";
+    echo "Dm G C;<br>";
+    echo "People fall in love in mysterious ways;<br>";
+    echo "Dm G;<br>";
+    echo "Maybe just the touch of a hand;<br>";
+    echo "Dm G Am;<br>";
+    echo "Well me I fall in love with you every single day;<br>";
+    echo "Dm G;<br>";
+    echo "And I just wanna tell you I am;<br>";
+    echo "C C/E;<br>";
+    echo "So honey now..<br>";
+    echo "<br>";
+    
+    echo "[Chorus]<br>";
+    echo "F G C C/E;<br>";
+    echo "Take me into your loving arms;<br>";
+    echo "F G C C/E;<br>";
+    echo "Kiss me under the light of a thousand stars;<br>";
+    echo "F G C C/E;<br>";
+    echo "Place your head on my beating heart;<br>";
+    echo "F G;<br>";
+    echo "I’m thinking out loud;<br>";
+    echo "Am G F C/E Dm G C;<br>";
+    echo "And maybe we found love right where we are;<br>";
+    echo "<br>";
+    
+    echo "[Verse]<br>";
+    echo "C C/E F G;<br>";
+    echo "And when my hair’s all but gone and my memory fades;<br>";
+    echo "C C/E F G;<br>";
+    echo "And the crowds don’t remember my name;<br>";
+    echo "C C/E F G;<br>";
+    echo "When my hands don’t play the strings the same way;<br>";
+    echo "C C/E F G;<br>";
+    echo "I know you will still love me the same;<br>";
+    echo "C C/E F G C C/E F G;<br>";
+    echo "Cause honey your soul could never grow old it’s evergreen;<br>";
+    echo "C C/E F G C C/E;<br>";
+    echo "And baby your smile’s forever in my mind and memory;<br>";
+    echo "F G;<br>";
+    echo "And I’m thinking bout how;<br>";
+    echo "<br>";
+    
+    echo "[Pre-Chorus]<br>";
+    echo "Dm G C;<br>";
+    echo "People fall in love in mysterious ways;<br>";
+    echo "Dm G;<br>";
+    echo "And maybe it’s all part of a plan;<br>";
+    echo "Dm G Am;<br>";
+    echo "Well I’ll just keep on making the same mistake;<br>";
+    echo "Dm G;<br>";
+    echo "Hoping that you’ll understand;<br>";
+    echo "C C/E;<br>";
+    echo "That baby now..<br>";
+    echo "<br>";
+    
+    echo "[Chorus]<br>";
+    echo "F G C C/E;<br>";
+    echo "Take me into your loving arms;<br>";
+    echo "F G C C/E;<br>";
+    echo "Kiss me under the light of a thousand stars;<br>";
+    echo "F G C C/E;<br>";
+    echo "Place your head on my beating heart;<br>";
+    echo "F G;<br>";
+    echo "I’m thinking out loud;<br>";
+    echo "<br>";
+    
+    echo "[Interlude]<br>";
+    echo "Am G F C/E Dm G C;<br>";
+    echo "That baby we found love right where we are;<br>";
+    echo "<br>";
+
+    echo "[Instrumental]<br>";
+    echo "C C/E F G;<br>";
+    echo "C C/E F G;<br>";
+    echo "C C/E F G;<br>";
+    echo "C C/E F G;<br>";
+    echo "<br>";
+
+    echo "[Chorus]<br>";
+    echo "C C/E;<br>";
+    echo "So baby now;<br>";
+    echo "F G C C/E;<br>";
+    echo "Take me into your loving arms;<br>";
+    echo "F G C C/E;<br>";
+    echo "Kiss me under the light of a thousand stars;<br>";
+    echo "F G C C/E;<br>";
+    echo "Place your head on my beating heart;<br>";
+    echo "F G;<br>";
+    echo "I’m thinking out loud;<br>";
+    echo "<br>";
+
+    echo "[Outro]<br>";
+    echo "Am G F C/E Dm G C;<br>";
+    echo "That maybe we found love right where we are;<br>";
+    echo "Am G F C/E Dm G C;<br>";
+    echo "Baby we found love right where we are;<br>";
+    echo "Am G F C/E Dm G C;<br>";
+    echo "And we found love right where we are;<br>";
+    echo "</pre>";
+    
+    ?>
+</body>
+</html>
