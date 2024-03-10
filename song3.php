2282723b8714e710b4146e62f5069cf89b707771
NunezJayson <nunez.jayson@mscmarinduque.edu.ph>
2024-03-10 18:46:57 +0800

    song 3

---
+++ song3.php
@@ -0,0 +1,144 @@
+<!-- song3.php -->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Song 3</title>
+</head>
+<body>
+    <h1>Song 3 - "I'm Yours" by Jason Mraz </h1>
+    <?php
+    echo "<p>Lyrics for 'I'm Yours' by Jason Mraz </p>";
+    echo "<pre>";
+    
+    echo "Lyrics and chords:<br>";
+    echo "<br>";
+    
+    echo "[Intro]<br>";
+    echo "Chords (relative to Capo):<br>";
+    echo "G D Em C<br>";
+    echo "3e fred<br>";
+    echo "<br>";
+    
+    echo "[Verse 1]<br>";
+    echo "G;<br>";
+    echo "Well you done done me and you bet I felt it,<br>";
+    echo "D;<br>";
+    echo "I tried to be chill but you’re so hot that I melted,<br>";
+    echo "Em;<br>";
+    echo "I fell right through the cracks,<br>";
+    echo "C;<br>";
+    echo "And now I’m tryin to get back…<br>";
+    echo "<br>";
+    
+    echo "[Verse 2]<br>";
+    echo "G;<br>";
+    echo "Before the cool done run out, I’ll be givin it my bestest<br>";
+    echo "D;<br>";
+    echo "and Nothin’s gonna stop me but divine intervention<br>";
+    echo "Em;<br>";
+    echo "I reckon it’s again my turn,<br>";
+    echo "C;<br>";
+    echo "To Win some or learn some…<br>";
+    echo "<br>";
+    
+    echo "[Chorus]<br>";
+    echo "G D;<br>";
+    echo "But I wont hesitate<br>";
+    echo "Em;<br>";
+    echo "No more, No more<br>";
+    echo "C G D Em C;<br>";
+    echo "It can not wait; I’m yours<br>";
+    echo "<br>";
+    
+    echo "[Verse 3]<br>";
+    echo "G D;<br>";
+    echo "Well, open up your mind and see like me,<br>";
+    echo "Em;<br>";
+    echo "Open up your plans and damn you’re free.<br>";
+    echo "C;<br>";
+    echo "Look into your heart and you’ll find love, love, love, love.<br>";
+    echo "G D Em;<br>";
+    echo "Listen to the music of the moment, people dance and sing, we’re just one big family<br>";
+    echo "C A7;<br>";
+    echo "And it’s our god-forsaken right to be loved, loved, loved, loved, loved.<br>";
+    echo "<br>";
+    
+    echo "[Chorus]<br>";
+    echo "G D;<br>";
+    echo "So I won’t hesitate<br>";
+    echo "Em;<br>";
+    echo "No more, no more<br>";
+    echo "C;<br>";
+    echo "It cannot wait,<br>";
+    echo "I’m sure<br>";
+    echo "<br>";
+    
+    echo "G D;<br>";
+    echo "There’s no need to complicate<br>";
+    echo "Em;<br>";
+    echo "Our time is short<br>";
+    echo "C;<br>";
+    echo "This is our fate,<br>";
+    echo "G;<br>";
+    echo "I’m yours<br>";
+    echo "<br>";
+    
+    echo "[Bridge]<br>";
+    echo "G Bm;<br>";
+    echo "Doo do do doo doo do, doo do doo do doo do<br>";
+    echo "Em D C;<br>";
+    echo "Do you want to come on, scooch on over closer, dear<br>";
+    echo "A7;<br>";
+    echo "And I will nibble your ear<br>";
+    echo "G Bm Em D C A7;<br>";
+    echo "<br>";
+    
+    echo "[Verse 4]<br>";
+    echo "G;<br>";
+    echo "I’ve been spending way too long checking my tongue in the mirror<br>";
+    echo "D;<br>";
+    echo "And bending over backwards just to try to see it clearer<br>";
+    echo "Em;<br>";
+    echo "But my breath fogged up the glass<br>";
+    echo "C;<br>";
+    echo "And so I drew a new face and I laughed.<br>";
+    echo "G;<br>";
+    echo "I guess what I’ll be saying is there ain’t no better reason<br>";
+    echo "D;<br>";
+    echo "To rid yourself of vanities and just go with the seasons.<br>";
+    echo "Em;<br>";
+    echo "It’s what we aim to do.<br>";
+    echo "C;<br>";
+    echo "Our name is our virtue.<br>";
+    echo "<br>";
+    
+    echo "[Chorus]<br>";
+    echo "G D;<br>";
+    echo "But I won’t hesitate<br>";
+    echo "Em;<br>";
+    echo "No more, No more<br>";
+    echo "C G;<br>";
+    echo "It can not wait; I’m yours<br>";
+    echo "<br>";
+
+    echo "[Outro]<br>";
+    echo "G D;<br>";
+    echo "Well, open up your mind and see like me,<br>";
+    echo "Em;<br>";
+    echo "Open up your plans and damn you’re free.<br>";
+    echo "C;<br>";
+    echo "Look into your heart and you’ll find that the sky is yours.<br>";
+    echo "G;<br>";
+    echo "So please don’t, please don’t, please don’t…<br>";
+    echo "D;<br>";
+    echo "There’s no need to complicate.<br>";
+    echo "Em;<br>";
+    echo "I’m yours…<br>";
+    echo "</pre>";
+
+    
+    ?>
+</body>
+</html>
