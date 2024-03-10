8cb232728bc28514e7da0909275df86c8b0cabdf
NunezJayson <nunez.jayson@mscmarinduque.edu.ph>
2024-03-10 18:44:35 +0800

    Index & Css

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Welcome to Jurie's playlist - Your Music Paradise</h1>
+    <ul>
+        <li><a href="song1.php">Song 1</a></li>
+        <li><a href="song2.php">Song 2</a></li>
+        <li><a href="song3.php">Song 3</a></li>
+        <li><a href="song4.php">Song 4</a></li>
+        <li><a href="song5.php">Song 5</a></li>
+    </ul>
+</body>
+</html>
